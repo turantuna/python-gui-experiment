7cd0b77d592df91037cf7d213686c9d0d2628e70
Tuna Turan <161838895+turantuna@users.noreply.github.com>
2025-11-21 02:02:01 +0100

    password generator

--- src/main.py
+++ src/main.py
@@ -1,13 +1,30 @@
 import tkinter as tk
 import random
 import string
-def generate_random_pass(length=12):
+
+def generate_password():
+    length = int(entry_length.get())        
     characters = string.ascii_letters + string.digits + string.punctuation
-    random_pass = ''.join(random.choice(characters) for i in range(length))
-    return random_pass
+    password = "".join(random.choice(characters) for _ in range(length))
+    entry_password.delete(0, tk.END)        
+    entry_password.insert(0, password)      
+
+
 root = tk.Tk()
-root.title("Random String Generator")
-root.geometry("400x200")
-button = tk.Button(root,text= "Generate Random String",command=generate_random_pass)
-button.pack()
+root.title("Password Generator")
+root.geometry("300x200")
+
+
+label_length = tk.Label(root, text="Password Length:")
+label_length.pack()
+
+entry_length = tk.Entry(root)
+entry_length.pack()
+
+button_generate = tk.Button(root, text="Generate Password", command=generate_password)
+button_generate.pack(pady=10)
+
+entry_password = tk.Entry(root, width=30)
+entry_password.pack()
+
 root.mainloop()
